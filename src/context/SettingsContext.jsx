9128df60ba3bd721fcd1db88a238fe6860110147
tomaszolejniczak77 <o.tomek@wp.pl>
2024-09-20 11:16:45 +0200

    add local storage for user settings

--- src/context/SettingsContext.jsx
+++ src/context/SettingsContext.jsx
@@ -1,9 +1,16 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const SettingsContext = createContext();
 
 export const SettingProvider = ({ children }) => {
-  const [userWindGust, setUserWindGust] = useState(8);
+  const [userWindGust, setUserWindGust] = useState(() => {
+    const savedValue = localStorage.getItem("userWindGust");
+    return savedValue !== null ? JSON.parse(savedValue) : 8;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("userWindGust", JSON.stringify(userWindGust));
+  }, [userWindGust]);
 
   return (
     <SettingsContext.Provider value={{ userWindGust, setUserWindGust }}>
